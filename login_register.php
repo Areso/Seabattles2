e9360f1deaf8ffacb2c125cf887c94c02ca7c606
areso <returnstrike@yandex.ru>
2018-05-01 22:16:43 +0500

    test git hook2

--- login_register.php
+++ login_register.php
@@ -1,3 +1,12 @@
 <?php
-
+connect db
+check user
+if doesn't exist
+  create new user
+if exists 
+  check password
+    if passord correct
+      login
+    else
+      redirect to passrestore page
 ?>
