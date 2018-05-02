8286e59d621bc75c9a3f3bb8da31bc992153463c
areso <returnstrike@yandex.ru>
2018-05-02 20:59:39 +0500

    test9

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $dbusername  ="root";
-$dbpassword="myverystrongpassword";
+$dbpassword="MySQL0000";
 $servername  = "localhost";
 $port        = "3306";
 $dbname      = "seabattles";
--- login_register.php
+++ login_register.php
@@ -2,7 +2,7 @@
 connect db
 check user
 if doesn't exist
-  create new user
+  create new user  
 if exists 
   check password
     if passord correct
