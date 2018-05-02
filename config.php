9e6417842210b73332dd294d234b5dfa2d9a4295
areso <returnstrike@yandex.ru>
2018-05-02 20:25:17 +0500

    test7

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $dbusername  ="root";
-$dbpassword="MySQL0000";
+$dbpassword="myverystrongpassword";
 $servername  = "localhost";
 $port        = "3306";
 $dbname      = "seabattles";
--- py2.py
+++ py2.py
@@ -1,14 +1,14 @@
 import sys
  
 
-f = open('config.php', 'r')
+f = open('~/Documents/Seabattles2/config.php', 'r')
 configtext  = f.read()
 passwordpos = configtext.find("password");
 newlinepos  = configtext.find("\n", passwordpos)
 passwordold = configtext[passwordpos+10:newlinepos-2]
 passwordnew = "test0000"
 f.close()
-f = open('config.php', 'w')
+f = open('~/Documents/Seabattles2/config.php', 'w')
 print(passwordpos)
 print(passwordold)
 print(configtext)
